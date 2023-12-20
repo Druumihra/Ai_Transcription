b2850fce86342a3bac7a5e176cae538ca9510ded
Druumihra <teis.bindner@laerke.net>
2023-12-20 08:47:26 +0100

    refactored some code

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import whispertranscribe
 import Audio
 import Subtitle
-import os
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
+
 
 
 language = ["ca", "en", "es"]
@@ -11,7 +9,7 @@ i = 0
 audio = Audio.Mp4ToMp3("Files/videofile.mp4")
 for lang in language:
     whispertranscribe.whisperSTT(audio, lang)
-    Subtitle.addSubtitle("Files/test.srt","Files/videofile.mp4", lang)
-    
+    if language == "en":
+        Subtitle.addSubtitle("Files/en-transcribtion.srt","Files/videofile.mp4", lang)
     i+1
 
--- whispertranscribe.py
+++ whispertranscribe.py
@@ -4,9 +4,7 @@ from datetime import timedelta
 
 def whisperSTT(audio, language):
     model = whisper.load_model("large-v3")
-    print(language)
     result = model.transcribe(audio, language=language)
-    print(result['text'])
     segments = result["segments"]
 
     for segment in segments:
@@ -15,7 +13,7 @@ def whisperSTT(audio, language):
         text = segment['text']
         id = segment['id']+1
         segment = f"{id}\n{startime} ---> {endtime}\n {text[1:] if text[0] is ' ' else text}\n\n"
-        srtfilename = os.path.join("Files", "test.srt")
+        srtfilename = os.path.join("Files", f"{language}-transcribtion.srt")
         with open(srtfilename, 'a', encoding='utf-8') as srtFile:
             srtFile.write(segment)
     return srtfilename
